c44c1d8e1664bcf9a866f3c3fbe24bfa9c83eba1
Arturo López <arturo.lopez@cesjuanpablosegundo.es>
2019-12-04 11:37:49 +0100

    resta

---
+++ RecylcerView/app/src/main/java/com/example/recylcerview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.recylcerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        instancias();
+        acciones();
+    }
+
+    private void acciones() {
+    }
+
+    private void instancias() {
+        recyclerView = findViewById(R.id.recycler_view);
+    }
+}
